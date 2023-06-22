19930da9082611d0af7cb06d77515dc5a65db940
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-06-22 14:54:53 +0900

    ✎[57page] 02.JSX > 2.4 JSX문법 > 2.4.2 자바스크립트 표현
    JSX 안에서는 자바스크립트 표현식을 쓸 수 있습니다. 자바스크립트 표현식을 작성하려면 JSX 내부에서 코드를 { }로 감싸면 된다.

---
+++ 02.JSX/57page_jsx.js
@@ -0,0 +1,15 @@
+import React ,{Component} from 'react';
+ 
+function App extends Coponent {
+  render(){   
+  const text = '리액트 !';
+  return (
+      <div>
+          <h1>리엑트 안녕!</h1>
+          <h2>{text}</h2>
+      </div>
+   );
+ }
+}
+ 
+export default App;
