69632ea6a5a15c3d71f8374b455fc27167aca0a2
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-06-22 17:44:47 +0900

    02.JSX > 2.4 JSX문법 > 2.4.6 class 대신 className
    class를 설정할 때는 class 키워드 대신 className으로 설정해야 된다.
    class키워드는 이미 자바스크립트에 존재 하는 키워드 이기 떄문이다.
    DOM API에서도  자바스크립트로 클래스를 설정할 때 className 키워드를 사용한다.

---
+++ 02.JSX/62page_jsx.js
@@ -0,0 +1,20 @@
+import React{Componrnt} from "react";
+import './62page_jsx.css'
+import { Component } from 'react';
+
+
+class App extends Component{
+    render(){
+        (...)
+        return(
+            <div class="my-div">
+                <h1>리엑트 공부</h1>
+                <h2>{Text}</h2>
+                {condition &&  '해주세여'}
+                <div style={style}></div>
+            </div>
+        );
+    }
+}
+
+export default App;
