054d425a845d632b0e0ad4dad88996547ff313c8
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-06-22 11:23:24 +0900

    48page    02.JSX  > 2.2 JSX란?
     A.jsx란?
    JSX는 자바스크립트의 확장 문법으로  XML과 매우 비슷하게 생겼다.
    
     ※JSX도 자바스크립트 문법이라고 할 수 있을까요?
    
    JSX는 리액트로 프로젝트를 개발할 때 사용되므로 공식적인 자바스크립트 문법이 아닙니다. 바벨에서는 여러 문법을 지원할 수 있도록 preset 및 plugin을 설정합니다. 바벨을 통해 개발자들이 임의로 만든 문법, 혹은 차기 자바스크립트의 문법들을 사용할 수 있습니다.

---
+++ 02.JSX/52page_jsx.js
@@ -0,0 +1,26 @@
+var a{
+    <div>
+        <h1>Awsome <b>react</b></h1>
+    </div>
+}
+
+
+//바벨로 변환
+var a = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "awsome",
+        React.createElement(
+            'b',
+            null,
+            "React"
+        )
+
+
+ㄴ
+    )
+)
+
