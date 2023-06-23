cd1c9130c2663780ad486732ccfb0194c9646a58
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-06-23 13:09:22 +0900

    ✎[71page] 02.Component > 3.1 첫 컴포넌트 생성 > 3.1.3 모듈 내보내기 및 불러오기
    App 컴포넌트에서 MyComponent 컴포넌트를 불러온다.

--- 03.Component/71page_component.js
+++ 03.Component/71page_component.js
@@ -1,6 +1,5 @@
-import { Component } from "react";
-import MyComponent  from'./70page_component';  //70page_component 파일을 불러온다
-
+import React, from 'react';{ Component } from "react";
+import MyComponent  from'./70page_component';  //70page_component 파일을 불러온다. 
 class App extends Component{
     render(){
         return(
