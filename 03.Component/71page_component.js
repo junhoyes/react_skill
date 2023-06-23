401e8884a7155b216798c4d13ece4d6eb5ec7297
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-06-23 13:01:56 +0900

    ✎[71page] 02.Component  > 3.1 첫 컴포넌트 생성 > 3.1.2 컴포넌트 초기 코드 작성

---
+++ 03.Component/71page_component.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import MyComponent  from'./70page_component';  //70page_component 파일을 불러온다
+
+class App extends Component{
+    render(){
+        return(
+            <MyComponent/>
+        );
+    }
+}
+
+export default App;
