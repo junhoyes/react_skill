b49506c8ae4000b588218110ab395d8b9e7b96c6
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-06-23 11:20:52 +0900

    ✎[70page] 02.Component  > 3.1 첫 컴포넌트 생성 > 3.1.2 컴포넌트 초기 코드 작성

---
+++ 03.Component/70page_jsx.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import React{Componrnt} from "react";
+
+class MyComponent extends Component{
+    render(){
+        return(
+            <div>
+                나의 새롭고 멋진 컴포넌트
+            </div>
+        )
+    }
+}
+export default MyComponent;
