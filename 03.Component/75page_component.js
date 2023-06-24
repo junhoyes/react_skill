f2fdae779f89bf2c8a22a27fa7dda45c186d912a
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-06-24 11:09:44 +0900

    ✎[75page] 02.Component > 3.2 props > 3.2.3 prop 기본 값 설정:defaultProps
    Class 문법을 사용하여 컴포넌트를 만들떄 코드를 간결하게 작성하면서 사용했다.

--- 03.Component/75page_component.js
+++ 03.Component/75page_component.js
@@ -7,7 +7,7 @@ class MyComponent extends Component{
     render(){
         return(
             <div>
-                안녕하세요 제이름은 {this.props.name}입니다.
+                안녕하세요 제이름은 {this.props.name}입니다. 
             </div>
 
         );
