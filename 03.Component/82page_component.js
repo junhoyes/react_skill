fed45eda201b22d4cb4a4c7805b48f692bd79dd3
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-06-24 16:50:48 +0900

    ✎[82page] 02.Component > 3.3 state > 3.3.1 컴포넌트 생성자 매서드:constructor()
    stare 초기값은 컴포넌트의 생성자 매서드인 constructor 내부에서 설정된다.
    생성자 매서드는 컴포넌트를 새로 만들떄 실행된다.
    
     MyComponent는 리엑트의 Component 클래스를 상속한다.
    따로 constructor 매서드를 만들어 주지 않으면 Component 클래스 생성자 매서드를 그대로 사용한다.
    직접  constructor 매서드를 작성하여 생성자 매서드에서 추가 작업을 하려면 매서드  내부에서 부모 클래스인  Component의 constructor 매서드를 먼저 호출해야된다.
    이떄 super 키워드를 사용한다.  컴포넌트를 만들떄 prop 값들을 사용하므로 prop를 매서드의 피라미터로 전달한다.

---
+++ 03.Component/82page_component.js
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+classs MyComponent extends Component{
+    (...)
+    Contructor(props){
+        super(props);
+    }
+
+    render(){
+        (...)
+    }
+}
+
+export default MyComponent;
+
