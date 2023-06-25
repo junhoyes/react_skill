c9b949e1cbb8998d72e366a1863a2e252ab1a9ae
박준호 <junhoyes92@naver.com>
2023-06-25 19:28:47 +0900

    ✎[83page] 02.Component > 3.3 state > 3.3.3 JSX내부에서 state 설정
    number값 랜더링

---
+++ 03.Component/83page_component.js
@@ -0,0 +1,10 @@
+render(){
+    return(
+        <div>
+            <p> 안녕하세요 제이름은 {this.proops.render}</p>
+            <p>저는 {this.props.age}살 입니다.</p>
+            <p>숫자:{this.StaticRange.number}</p>
+        </div>
+    )
+}
+
