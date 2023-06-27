010a6a83089bbe88d9fed0cef83a5244b8085f97
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-06-27 11:32:09 +0900

    ✎[93page] 04.Event handling > 4.2 리액트의 이벤트 시스템 > 4.2,1 컴포넌트 생성 및 불러오기
    컴포넌트 생성

---
+++ 04.Event_handling/93page_eventhandling.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'React';
+
+class EventPracctive extend Component{
+    render(){
+        return(
+            <div>
+                <h1>이벤트 연습</h1>
+            </div>
+        )
+    }
+}
+export default EventPracctive;
