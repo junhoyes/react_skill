04d5787b74c1078cb85e110b841067f1f6e03347
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-06-27 14:14:59 +0900

    ✎[94page] 04.Event handling > 4.2 리액트의 이벤트 시스템 > 4.2,1 컴포넌트 생성 및 불러오기
     EventPractice 불러온후에  component 랜더링

---
+++ 04.Event_handling/94page_eventhandling.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'React';
+import EventPractice  from './93page_eventhandling';
+
+class App extends Component{
+    render(){
+        return(
+            <EventPractice/>
+        );
+    }
+}
+export default App;
