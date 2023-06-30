41be85696f17dd22eec8fa7e8717e1e466a41792
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-06-30 13:46:30 +0900

    ✎[102page] 05 ref: DOM에 이름 달기 > 5.1 ref는 어떤 상황에서 사용해야 할까?  > 5.1.1예제 컴포넌트 생성
    input에서는 onChange 이벤트가 발생하면  handleChange를 호출하여서 state의 password 값을 업데이트 하게 했다.
    button에서 onClick 이벤트가 발생하면 handleButtonClick을 호출하여 clicked값을 검증 결과로 설정했다.
    
    input의 className값은 버튼을 누르기 전까지 비어 있는 문자열을 전달하며 버튼을 누른후에는 검증 결과에 따라 success 값 또는 failure값을 설정한다.
    이값에 따라서 input 색상이 초록색 또는 빨강색으로 나타난다.

---
+++ 05.ref_DOM에 이름 달기/113page_ref-dom_name.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'React';
+import './113page_ref-dom_name.css';
+
+class EventPractice extends Component{
+    state={
+        password:'',
+        click:false,
+        validated:false        
+    }
+
+
+    handleChange = (e) => {
+        this.setState({
+            password:e.target.value
+        });
+    }
+
+    handleButtonClick=() => {
+        this.setState({
+            clicked:true,
+            validated:this.state.password === '0000'
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <input
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') :''}
+                />
+                <button onClick={this.handleButtonClick}>검증하기</button>
+            </div>
+        );
+    }
+    
+}
+
+export default validationSample;
