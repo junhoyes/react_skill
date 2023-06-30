792de3edc7ffa73fba5b1a4e84c678cc54c06e2d
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-06-30 15:37:15 +0900

    ✎[113page] 05 ref: DOM에 이름 달기 > 5.1 ref는 어떤 상황에서 사용해야 할까? > 5.1.2 App 컴포넌트에서 예제 컴포넌트 랜더링
    ValidationSample 컴포넌트를 불러와 랜더링을 한다.
    
    DOM을 꼭 사용해야 하는 상황
    
    앞 예제에서는 state를 사용하여 우리에게 필요한 기능을 구현했지만, 가끔 state만으로 해결할 수 없는 기능이 있다.
    
    • 특정 input에 포커스 주기
    
    • 스크롤 박스 조작하기
    
    • Canvas 요소에 그림 그리기 등
    
    이때는 어쩔 수 없이 DOM에 직접적으로 접근해야 하는데, 이를 위해 바로 ref를 사용합니다

---
+++ 05.ref_DOM에 이름 달기/114page_ref-dom_name.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'React';
+import './113page_ref-dom_name.css';
+
+class EventPractice extends Component{
+    render(){
+        return(
+            <validationSample/>
+        );
+    }
+}
+
+export default  App;
