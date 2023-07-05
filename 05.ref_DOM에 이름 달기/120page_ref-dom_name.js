a8f0d66ba082b37b4c82ee3ba95d6a9a4d751cd6
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-07-05 10:59:00 +0900

    ✎[120page] 05 ref: DOM에 이름 달기 > 5.3 컴포넌트에 ref 달기 > 5.3.2. 컴포넌트 초기설정 > 5.3.2.2 컴포넌트 스크롤 박스 컴포넌트 랜더링

---
+++ 05.ref_DOM에 이름 달기/120page_ref-dom_name.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import ScrollBox from'./119page_ref-dom_name';
+
+
+class  App extends Component{
+    render(){
+        return(
+            <div>
+                <scrollBox/>
+            </div>
+
+        );
+    }
+}
+ 
+export default ScrollBox;
