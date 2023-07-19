aa03328bafc4902e187a0694fafc38577762581e
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-07-19 15:42:20 +0900

    ✎[130page] 06.Repeated_Components > 6.2 데이터 배열을 컴포넌트 배열로 map 하기 > 6.2.2.  App 컴포넌트에서 예제 컴포넌트 랜더링
    원하는 대로 렌더링이 되었지만... 하지만 아직 완벽하지는 않는다..
    그이유는  콘솔창에  "key" prop이 없다는 경고 메시지를 표시가 되어있다.

---
+++ 06.Repeated_Components/130page_repeated_components.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import IterationSample from'./129page_repeated_component';
+
+class App extends Component{
+    render(){
+        return(
+            < IterationSample/>
+        )
+    }
+}
+ 
+export default App;
