560568230981311d2ed56c1662320a94093ab876
junhoyes <43289515+junhoyes@users.noreply.github.com>
2023-07-20 09:49:03 +0900

    ✎[131page] 06.Repeated_Components > 6.3 key > 6.3.1 key 설정
    고유한 값이 없을 때만 index 값을 key로 사용해야 합니다. index를 key로 사용하면 배열이 변경될 때 효율적으로 리렌더링하지 못합니다

--- 06.Repeated_Components/131page_repeated_components.js
+++ 06.Repeated_Components/131page_repeated_components.js
@@ -4,7 +4,6 @@ const IterationSample = () => {
   const names = ['눈사람', '얼음', '눈', '바람'];
   const nameList = names.map(
     (name, index) => <li key={index}>{name}</li>);
-
     return(
         <ul>
             {nameList}
